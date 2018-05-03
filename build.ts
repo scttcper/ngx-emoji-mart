ade790100f6adfbe43cea8a4af0944732ddb785f
Scott Cooper <scttcper@gmail.com>
2018-05-03 00:05:05 -0700

    build: use node_modules as working dir

--- build.ts
+++ build.ts
@@ -1,48 +1,55 @@
-
 import { copySync } from 'fs-extra';
-import { build } from 'ng-packagr';
+import { ngPackagr } from 'ng-packagr';
 import { join } from 'path';
 import * as rimraf from 'rimraf';
-
-
 
 async function main() {
   // cleanup dist
   rimraf.sync(join(process.cwd(), '/dist'));
+  rimraf.sync(join(process.cwd(), '/node_modules/@ctrl/ngx-emoji-mart'));
 
   // make emoi
-  await build({
-    project: join(process.cwd(), 'src/lib/emoji/package.json'),
-  });
+  await ngPackagr()
+    .forProject(join(process.cwd(), 'src/lib/emoji/package.json'))
+    .build();
 
-
-  rimraf.sync(join(process.cwd(), `/src/lib/picker/node_modules`));
+  // put it in node modules so the path resolves
+  // proper path support eventually
   copySync(
-    join(process.cwd(), '/dist/ngx-emoji'),
-    join(process.cwd(), `/src/lib/picker/node_modules/@ctrl/ngx-emoji-mart/ngx-emoji`),
+    join(process.cwd(), '/dist/emoji'),
+    join(process.cwd(), '/node_modules/@ctrl/ngx-emoji-mart/ngx-emoji'),
   );
-  await build({
-    project: join(process.cwd(), `src/lib/picker/package.json`),
-  });
+  copySync(
+    join(process.cwd(), '/dist/emoji'),
+    join(process.cwd(), '/dist/packages-dist/ngx-emoji'),
+  );
+
+  await ngPackagr()
+    .forProject(join(process.cwd(), 'src/lib/picker/package.json'))
+    .build();
+
   copySync(
     join(process.cwd(), '/dist/picker'),
     join(process.cwd(), '/dist/packages-dist'),
   );
-  copySync(
-    join(process.cwd(), '/dist/ngx-emoji'),
-    join(process.cwd(), '/dist/packages-dist/ngx-emoji'),
-  );
 
-  copySync(join(process.cwd(), 'README.md'), join(process.cwd(), 'dist/packages-dist/README.md'));
-  copySync(join(process.cwd(), 'LICENSE'), join(process.cwd(), 'dist/packages-dist/LICENSE'));
-  copySync(join(process.cwd(), 'src/lib/picker/picker.css'), join(process.cwd(), 'dist/packages-dist/picker.css'));
+  copySync(
+    join(process.cwd(), 'README.md'),
+    join(process.cwd(), 'dist/packages-dist/README.md'),
+  );
+  copySync(
+    join(process.cwd(), 'LICENSE'),
+    join(process.cwd(), 'dist/packages-dist/LICENSE'),
+  );
+  copySync(
+    join(process.cwd(), 'src/lib/picker/picker.css'),
+    join(process.cwd(), 'dist/packages-dist/picker.css'),
+  );
 }
 
 main()
   .then(() => console.log('success'))
-  .catch((e) => {
+  .catch(e => {
     console.error(e);
     process.exit(1);
   });
-
-
