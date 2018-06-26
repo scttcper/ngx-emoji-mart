4dc52e52bbe7b103b834473c924a92a2545aa2f4
Scott Cooper <scttcper@gmail.com>
2018-06-25 19:44:48 -0700

    chore: mock button service count

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,24 @@
 import { async, TestBed } from '@angular/core/testing';
 
-import { NtkmeButtonModule } from '@ctrl/ngx-github-buttons';
+import { ButtonService, NtkmeButtonModule } from '@ctrl/ngx-github-buttons';
+import { of as ObservableOf } from 'rxjs';
 
 import { PickerModule } from '../lib/picker/picker.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer.component';
 
+class FakeButtonService {
+  repo(user: string, repo: string) {
+    return ObservableOf({ stargazers_count: 0 });
+  }
+}
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        PickerModule,
-        NtkmeButtonModule,
-      ],
-      declarations: [
-        AppComponent,
-        FooterComponent,
-      ],
+      imports: [PickerModule, NtkmeButtonModule],
+      declarations: [AppComponent, FooterComponent],
+      providers: [{ provide: ButtonService, useClass: FakeButtonService }],
     }).compileComponents();
   }));
   it('should create the app', async(() => {
