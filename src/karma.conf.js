1df3083d860b5491224c71be34c1b6b7088c2c5a
Scott Cooper <scttcper@gmail.com>
2018-09-05 23:13:30 -0700

    chore: fix karma path

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -17,12 +17,9 @@ module.exports = function(config) {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), 
+      dir: require('path').join(__dirname, '../coverage'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true,
-    },
-    angularCli: {
-      environment: 'dev',
     },
     reporters: ['mocha', 'kjhtml'],
     port: 9876,
