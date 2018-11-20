dbfcabd0466064022680430daf2b905485e03e9e
Scott Cooper <scott.cooper@zumepizza.com>
2018-11-19 23:04:13 -0800

    chore: add es7 array

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -33,6 +33,7 @@ import 'core-js/es6/set';
 import 'core-js/es6/string';
 import 'core-js/es6/symbol';
 import 'core-js/es6/weak-map';
+import 'core-js/es7/array';
 
 /**
  * If the application will be indexed by Google Search, the following is required.
