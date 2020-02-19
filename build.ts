f76c9e220a1ea0768c691d98c6abe24195aaebe5
Scott Cooper <scttcper@gmail.com>
2020-02-19 08:41:24 -0800

    build: use ng-packagr to auto discover emoji submodule

--- build.ts
+++ build.ts
@@ -6,44 +6,22 @@ import * as del from 'del';
 async function main() {
   // cleanup dist
   del.sync(join(process.cwd(), '/dist'));
-  del.sync(join(process.cwd(), '/node_modules/@ctrl/ngx-emoji-mart'));
-
-  // make emoi
-  await ngPackagr()
-    .forProject(join(process.cwd(), 'src/lib/emoji/package.json'))
-    .build();
-
-  // put it in node modules so the path resolves
-  // proper path support eventually
-  copySync(
-    join(process.cwd(), '/dist/emoji'),
-    join(process.cwd(), '/node_modules/@ctrl/ngx-emoji-mart/ngx-emoji'),
-  );
-  copySync(
-    join(process.cwd(), '/dist/emoji'),
-    join(process.cwd(), '/dist/packages-dist/ngx-emoji'),
-  );
 
   await ngPackagr()
     .forProject(join(process.cwd(), 'src/lib/picker/package.json'))
     .build();
 
   copySync(
-    join(process.cwd(), '/dist/picker'),
-    join(process.cwd(), '/dist/packages-dist'),
-  );
-
-  copySync(
     join(process.cwd(), 'README.md'),
-    join(process.cwd(), 'dist/packages-dist/README.md'),
+    join(process.cwd(), 'dist/README.md'),
   );
   copySync(
     join(process.cwd(), 'LICENSE'),
-    join(process.cwd(), 'dist/packages-dist/LICENSE'),
+    join(process.cwd(), 'dist/LICENSE'),
   );
   copySync(
     join(process.cwd(), 'src/lib/picker/picker.css'),
-    join(process.cwd(), 'dist/packages-dist/picker.css'),
+    join(process.cwd(), 'dist/picker.css'),
   );
 }
 
