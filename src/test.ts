15d80aef3d5b6ceb5ef3220628eee12aede84f9a
Scott Cooper <scttcper@gmail.com>
2020-07-01 22:17:14 -0700

    feat: upgrade to angular 10 BREAKING CHANGE: requires angular >10

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-/* tslint:disable:ordered-imports */
+
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
@@ -7,7 +7,16 @@ import {
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
 
-declare const require: any;
+declare const require: {
+  context(
+    path: string,
+    deep?: boolean,
+    filter?: RegExp,
+  ): {
+    keys(): string[];
+    <T>(id: string): T;
+  };
+};
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
