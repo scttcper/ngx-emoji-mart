e832b0665792ca385e17ad2072305055e73515e2
Scott Cooper <scttcper@gmail.com>
2023-05-08 21:00:20 -0700

    try again

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     customLaunchers: {
-      ChromeHeadless: {
+      ChromeHeadlessCustom: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox', '--disable-gpu'],
       },
