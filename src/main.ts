52cf036efd919e2ca5310d08038412a70b1a2ac9
arturovt <arthurandrosovich@gmail.com>
2023-05-15 21:31:11 +0300

    ci: setup Cypress tests

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
+import { setupGoogleAnalytics } from './ga';
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
 
-if (environment.production) {
-  enableProdMode();
+// Do not enable any tracking until it's deployed to Vercel.
+if (environment.vercel) {
+  setupGoogleAnalytics();
 }
 
 bootstrapApplication(AppComponent);
